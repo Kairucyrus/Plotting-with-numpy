b6afed16e57b6140afb53c878055aaeafa21c006
Kairu Cyrus <97586801+Kairucyrus@users.noreply.github.com>
2023-03-17 23:38:52 +0300

    Add files via upload

---
+++ plotplottests.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 17 23:25:33 2023
+
+@author: Kairu Cyrus
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+x= np.linspace(-10,10,100)
+y= np.sin(x)
+plot=plt.plot(x,y)
+plot.show()

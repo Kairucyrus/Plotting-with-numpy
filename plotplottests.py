01018fb80757648cdf8161ace7b0d0be30dee59f
Kairu Cyrus <97586801+Kairucyrus@users.noreply.github.com>
2023-04-15 15:57:52 +0300

    Update plotplottests.py

--- plotplottests.py
+++ plotplottests.py
@@ -7,7 +7,7 @@ Created on Fri Mar 17 23:25:33 2023
 
 import matplotlib.pyplot as plt
 import numpy as np
-x= np.linspace(-10,10,100) # Start point -10, stop-point 10(excluded unless endpoint=True), 100 samples
+x= np.linspace(-10,10,100) # Start point -10, stop-point 10(included unless endpoint=False), 100 samples
 y= np.sin(x)
 plot=plt.plot(x,y)
 plot.show()
